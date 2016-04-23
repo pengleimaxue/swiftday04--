db88e8f5634a7264fee1e99ae710aae4dcc1613c
万安 <wanan@wanandemacbook-pro-2.local>
2016-04-23 15:51:02 +0800

    Initial Commit

---
+++ swiftday04-函数Tests/swiftday04___Tests.swift
@@ -0,0 +1,36 @@
+//
+//  swiftday04___Tests.swift
+//  swiftday04-函数Tests
+//
+//  Created by 万安 on 16/4/23.
+//  Copyright © 2016年 penglei. All rights reserved.
+//
+
+import XCTest
+@testable import swiftday04___
+
+class swiftday04___Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
